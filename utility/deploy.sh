66b989e9f4b3643cb84f23ef8e52409210303c78
Danil-114195722 <ejyou.user@gmail.com>
2024-04-28 23:39:56 +0300

    Added handlers for "status", "set-config" and "add" commands

--- utility/deploy.sh
+++ utility/deploy.sh
@@ -1,4 +1,4 @@
-basedir=$(dirname "$(realpath "$0")")
+basedir=$(dirname "$(dirname "$(realpath "$0")")")
 
 red_text="\033[31m"
 yellow_text="\033[33m"
@@ -27,6 +27,7 @@ function print_doc {
     printf "\t%-25s %-15s\n" "deploy status" "Show detailed status of \"deploy utility\"."
     printf "\t%-25s %-15s\n" "deploy set-config" "Start interactive mode for configure project dir."
     printf "\t%-25s %-15s\n" "deploy add" "Start interactive mode for adding custom script."
+
     echo -e "\nDescription:\n"
 
     exit 0
@@ -45,7 +46,6 @@ function bad_command_error() {
 
 
 function handle_command_run() {
-    echo -e "\nAAAAAAAAAA\n"
     echo "command_verbose=$command_verbose"
     echo "script_to_run=$script_to_run"
 
@@ -53,21 +53,65 @@ function handle_command_run() {
 }
 
 function handle_command_status() {
-    echo "status"
+    printf "Project dir for default deploy script:\n\t"
+    cat "$basedir"/utility/config
+
+    scripts_list=( $(ls -A "$basedir"/custom_scripts) )
+    printf "Custom scripts list:\n"
+    for arr_index in ${!scripts_list[*]}
+    do
+        printf "\t%s\n" "${scripts_list["$arr_index"]}"
+    done
+    echo
+
+    printf "Dir contains \"deploy utility\" runner:\n\t"
+    echo "$basedir"/utility
+
+    printf "Dir contains custom scripts:\n\t"
+    echo -e "$basedir/custom_scripts\n"
+
+    printf "Full path to log file:\n\t"
+    echo "$basedir"/logs/all-deploy.log
 
     exit 0
 }
 
 function handle_command_set_config() {
-    echo "set-config"
+    echo "Set project dir for default deploy script..."
+    read -p "Enter full path to your project: " -r full_path_to_project
 
-    exit 0
+    echo "project_dir=\"$full_path_to_project\"" > "$basedir"/config
+
+    # command exit status
+    command_status=$?
+    # something wrong
+    if [ "$command_status" != '0' ]; then
+        echo -e "${red_text}Something went wrong!!! Please, check your project dir \"$full_path_to_project\"${default_text}"
+        exit 1
+    # alright
+    else
+        echo -e "${green_text}Successfully configured default project dir!${default_text}"
+        exit 0
+    fi
 }
 
 function handle_command_add() {
-    echo "add"
+    echo "Adding new custom script..."
+    read -p "Enter path to your script file: " -r path_to_new_custom_command
 
-    exit 0
+    cp "$path_to_new_custom_command" "$basedir"/../custom_scripts/"$(basename "$path_to_new_custom_command")"
+
+    # command exit status
+    command_status=$?
+    # something wrong
+    if [ "$command_status" != '0' ]; then
+        echo -e "${red_text}Something went wrong!!! Please, check your file \"$path_to_new_custom_command\"${default_text}"
+        exit 1
+    # alright
+    else
+        echo -e "${green_text}Successfully added new custom script!${default_text}"
+        exit 0
+    fi
 }
 
 # print instruction if run script without argument
